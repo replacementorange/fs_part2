ccfcc7b8755433c0233513d1b3848b2f70f1ae91
replacementorange <notnenar@proton.me>
2024-05-17 13:47:23 +0300

    2.9*: The Phonebook Step 4

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -11,12 +11,15 @@ const Person = ({ person }) => {
 // Main app
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567'}
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   // Add new person
   const addPerson = (event) => {
@@ -50,9 +53,15 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with<input value={newFilter} onChange={handleFilterChange} /></div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handlePersonChange}/></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
@@ -62,8 +71,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person =>
-          <Person key={person.name} person={person}/>  
+        {persons.filter(char => char.name.toLowerCase() // filters name with chars trough filet
+        .includes(newFilter.toLocaleLowerCase()))
+        .map(person => <Person key={person.name} person={person}/> // map filtered names & show them 
         )}
       </div>
     </div>
